e88f6332e9935e0baec6f338bce2707934fe22dd
konradcr <63312007+konradcr@users.noreply.github.com>
2022-03-07 09:17:47 +0100

    Add Create Consultant for Admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,10 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Consultant;
+use App\Form\CreateConsultantType;
 use App\Repository\ConsultantRepository;
+use App\Security\AppCustomAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class AdminController extends AbstractController
 {
@@ -14,6 +21,40 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', [
             'consultants' => $consultantRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/admin/create-consultant', name: 'app_admin_create_consultant')]
+    public function createConsultant(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, AppCustomAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+        $consultant = new Consultant();
+        $form = $this->createForm(CreateConsultantType::class, $consultant);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $consultant->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $consultant,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            $consultant->setRoles(["ROLE_CONSULTANT"]);
+            $entityManager->persist($consultant);
+            $entityManager->flush();
+
+
+           return $this->redirectToRoute('app_admin');
+        }
+        return $this->render('admin/create-consultant.html.twig', [
+            'createConsultantForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/admin/profile', name: 'app_admin_profile')]
+    public function adminProfile(): Response
+    {
+        return $this->render('admin/profile.html.twig', [
         ]);
     }
 }
--- src/Form/CreateConsultantType.php
+++ src/Form/CreateConsultantType.php
@@ -2,32 +2,21 @@
 
 namespace App\Form;
 
-use App\Entity\User;
+use App\Entity\Consultant;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-class RegistrationFormType extends AbstractType
+class CreateConsultantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'label' => 'J\'accepte les conditions d\'utilisations.',
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You devez accepter les conditions d\'utilisation.',
-                    ]),
-                ],
-            ])
             ->add('plainPassword', RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -56,7 +45,7 @@ class RegistrationFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            'data_class' => Consultant::class,
         ]);
     }
 }
